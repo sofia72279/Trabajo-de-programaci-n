0419afaed36bd678bddbdc0111436d7b6e7ba557
Stepha_dz <stephanie.tatiana113@gmail.com>
2026-04-25 23:07:47 -0600

    Add files via upload

---
+++ 02_Codigo/MenuPrincipal.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace SistemaBiblioteca
+{
+    partial class MenuPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.btnAutor = new System.Windows.Forms.Button();
+            this.btnCategoria = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.BackColor = System.Drawing.SystemColors.Window;
+            this.lblTitulo.Location = new System.Drawing.Point(280, 78);
+            this.lblTitulo.MaximumSize = new System.Drawing.Size(0, 60);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(191, 20);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Sistema CRUD Biblioteca";
+            this.lblTitulo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnAutor
+            // 
+            this.btnAutor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnAutor.Location = new System.Drawing.Point(109, 223);
+            this.btnAutor.Name = "btnAutor";
+            this.btnAutor.Size = new System.Drawing.Size(125, 52);
+            this.btnAutor.TabIndex = 1;
+            this.btnAutor.Text = "Autores";
+            this.btnAutor.UseVisualStyleBackColor = false;
+            this.btnAutor.Click += new System.EventHandler(this.btnAutor_Click);
+            // 
+            // btnCategoria
+            // 
+            this.btnCategoria.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnCategoria.Location = new System.Drawing.Point(312, 223);
+            this.btnCategoria.Name = "btnCategoria";
+            this.btnCategoria.Size = new System.Drawing.Size(122, 52);
+            this.btnCategoria.TabIndex = 2;
+            this.btnCategoria.Text = "Categorías";
+            this.btnCategoria.UseVisualStyleBackColor = false;
+            this.btnCategoria.Click += new System.EventHandler(this.btnCategoria_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnSalir.Location = new System.Drawing.Point(506, 223);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(122, 52);
+            this.btnSalir.TabIndex = 3;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // MenuPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnCategoria);
+            this.Controls.Add(this.btnAutor);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "MenuPrincipal";
+            this.Text = "Menú Principal-Biblioteca";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button btnAutor;
+        private System.Windows.Forms.Button btnCategoria;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ 02_Codigo/MenuPrincipal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaBiblioteca
+{
+    public partial class MenuPrincipal : Form
+    {
+        public MenuPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAutor_Click(object sender, EventArgs e)
+        {
+            FormAutor autor = new FormAutor();
+            autor.ShowDialog();
+        }
+
+        private void btnCategoria_Click(object sender, EventArgs e)
+        {
+            FormCategoria categoria = new FormCategoria();
+            categoria.ShowDialog();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
